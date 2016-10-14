04005eb0696f91fd5514bc2d19a22ed85523d6a9
Graham Turner <turnertgraham@gmail.com>
2016-10-14 17:26:55 -0400

    lesson_plans_controller index tests

--- app/controllers/lesson_plans_controller.rb
+++ app/controllers/lesson_plans_controller.rb
@@ -1,2 +1,13 @@
 class LessonPlansController < ApplicationController
+
+  # Gets all lesson plans for all teachers and returns them in JSON object
+  def index
+    @lessons = LessonPlan.all
+
+    respond_to do |format|
+      format.json { render json: @lessons }
+    end
+
+  end
+
 end
