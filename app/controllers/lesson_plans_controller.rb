27a4a666c8158df75c93927a45e40295cc5fbdb4
Graham Turner <turnertgraham@gmail.com>
2016-10-14 18:12:41 -0400

    lesson_plan create tests

--- app/controllers/lesson_plans_controller.rb
+++ app/controllers/lesson_plans_controller.rb
@@ -10,4 +10,26 @@ class LessonPlansController < ApplicationController
 
   end
 
+  def create
+    @lesson_plan = current_teacher.lesson_plans.build(lesson_plan_params)
+    respond_to do |format|
+      if @lesson_plan.save
+        format.json { render json: @lesson_plan }
+      else
+        format.json { render json: {
+                                    errors: @lesson_plan.errors.full_messages },
+                                    :status => 422
+                                   }
+      end
+    end
+  end
+
+  private
+
+    def lesson_plan_params
+      params.require(:lesson_plan).permit(:title, :content, :hours,
+                                          :version, :state, :grade,
+                                          :subject, :lesson_type)
+    end
+
 end
--- spec/controllers/lesson_plans_spec.rb
+++ spec/controllers/lesson_plans_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 describe LessonPlansController do
 
-  let(:lesson_plan){create :lesson_plan}
   let(:teacher){create :teacher}
 
 
   describe 'GET #index' do
 
+    let!(:lesson_plan){create :lesson_plan}
+
     before do
-      lesson_plan
       sign_in teacher
       get :index, :format => :json
     end
@@ -27,6 +27,75 @@ describe LessonPlansController do
       data = JSON.parse(response.body)
       expect(data.length).to eq 1
     end
+
+  end
+
+  describe "POST #create" do
+
+
+    before do
+      sign_in teacher
+    end
+
+    it 'will return unsucessful response with invalid attributes' do
+      process :create, method: :post,
+              params: {
+                        :lesson_plan => attributes_for(:lesson_plan,
+                                                       :without_title)
+                      }, :format => :json
+
+      expect(response).to_not be_success
+    end
+
+    it 'will not create a lesson plan with invalid attributes' do
+      process :create, method: :post,
+              params: {
+                        :lesson_plan => attributes_for(:lesson_plan,
+                                                       :without_title)
+                      }, :format => :json
+
+      expect(LessonPlan.all).to be_empty
+    end
+
+    it 'will return error message on failed creation' do
+      process :create, method: :post,
+              params: {
+                        :lesson_plan => attributes_for(:lesson_plan,
+                                                       :without_title)
+                      }, :format => :json
+      body = JSON.parse(response.body)
+
+      expect(body['errors']).to_not be_empty
+    end
+
+    it 'will return succesful response with valid attributes' do
+      process :create, method: :post,
+              params: { :lesson_plan => attributes_for(:lesson_plan) },
+              :format => :json
+
+      expect(response).to be_success
+    end
+
+    it 'will create lesson in database with valid attributes' do
+      expect {
+      process :create, method: :post,
+              params: { :lesson_plan => attributes_for(:lesson_plan) },
+              :format => :json
+      }.to change{LessonPlan.count}.by(1)
+    end
+
+    it 'will return same created object with valid attributes' do
+      process :create, method: :post,
+              params: { :lesson_plan => attributes_for(:lesson_plan,
+                                                      title: "Graham Rocks!") },
+              :format => :json
+
+      new_lesson = JSON.parse(response.body)
+
+      expect(new_lesson['title']).to eq "Graham Rocks!"
+    end
+
+
 
   end
 
