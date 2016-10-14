ba9fe9ab3feee28e55ddfa9d7961b265d83aef15
Philip Cole Johnson <philipcolejohnson@gmail.com>
2016-10-14 15:24:18 -0700

    Adding new lesson page

--- app/assets/javascripts/lesson_app/services/markdown_service.js
+++ app/assets/javascripts/lesson_app/services/markdown_service.js
@@ -2,7 +2,10 @@ Lesson.factory('MarkdownService', ['$document', function($document) {
 
   var markdownService = {};
   var _editor = new SimpleMDE({
-    element: $document.find('#editor');
+    element: $document.find('#editor')[0],
+    spellChecker: false,
+    placeholder: "Lesson plan...",
+    status: false
   });
 
   markdownService.getEditor = function() {
