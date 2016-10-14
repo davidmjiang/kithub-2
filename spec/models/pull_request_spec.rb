dff41d0d2d0bbd1ad429cbca1e8e256ac23dfbda
Hannah Squier <hannahsquier@gmail.com>
2016-10-14 15:57:29 -0700

    add pull request model tests

--- spec/models/pull_request_spec.rb
+++ spec/models/pull_request_spec.rb
@@ -1,22 +1,28 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# #runs two tests to check if course is valid with a title and invalid without title
-# describe PullRequest do
+#runs two tests to check if assignment is valid with a title and invalid without title
+describe PullRequest do
 
-#   let(:pull_request){ create(:pull_request) }
+  let(:pull_request){ create(:pull_request) }
 
-#   it "is valid with default attributes" do
-#     expect(pull_request).to be_valid
-#   end
+  it "is valid with default attributes" do
+    expect(pull_request).to be_valid
+  end
 
-#   it "is invalid without default attributes" do
-#     sad_course = build(:course, :without_title)
-#     expect(sad_course).to_not be_valid
-#   end
+  it "is invalid without parent_plan" do
+    sad_assignment = build(:pull_request, :no_parent_plan)
+    expect(sad_assignment).to_not be_valid
+  end
 
-#   #testing associations
-#   it { should belong_to(:teacher) }
-#   it { should have_many(:students) }
-#   it { should have_many(:assignments) }
+  it "is invalid without forked_plan" do
+    sad_assignment = build(:pull_request, :no_forked_plan)
+    expect(sad_assignment).to_not be_valid
+  end
 
-# end
+  #testing associations
+  it { should belong_to(:parent_plan) }
+
+  it { should belong_to(:forked_plan) }
+  it { should have_many(:comments) }
+
+end
