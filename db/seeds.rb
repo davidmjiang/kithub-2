6e668d51cc5ead0e37168afb1b72719f9fe87f5d
Leo Ahnn <leo.ahnn@gmail.com>
2016-10-14 12:25:10 -0400

    working on creating standards for seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,11 +4,14 @@ Course.destroy_all
 Assignment.destroy_all
 Student.destroy_all
 Submission.destroy_all
+Student.destroy_all
 
 
 
 teachers = ['mike@gmail.com', 'matt@gmail.com', 'graham@gmail.com', 'david@gmail.com', 'hannah@gmail.com', 'alex@gmail.com', 'dylan@gmail.com', 'leo@gmail.com', 'phil@gmail.com']
 assignment_type = ['test', 'quiz', 'homework', 'project']
+subjects = ['Geometry', 'Home Economics', 'English', 'Spanish', 'Physics', 'Chemistry', 'Trigonometry', 'Art', 'Computer Science']
+
 
 puts 'creating teachers'
 teachers.each do |person|
@@ -27,5 +30,22 @@ t = Teacher.create(email: person,
   end
 end
 
+puts 'creating students'
+40.times do |i|
+  s = Student.create(first_name: Faker::Name.first_name,
+                     last_name: Faker::Name.last_name,
+                     email: Faker::Internet.safe_email( '#{i}' ))
+  randCourse = Course.all.sample
+  randAssignment = Assignment.where(course_id: randCourse.id).sample
+  s.Submission.create(assignment_id: randAssignment.id,
+                      raw_score: rand(0..randAssignment.total_points ))
+end
 
-cd
+
+puts 'creating standards'
+
+4.times do
+  Standard.create(title: Faker::Hipster.word,
+                  state: Faker::Address.state,
+                  subject: subjects.sample)
+end
