70e5f56ce99e53083233349d99e0521959924b79
Alex Lach <alexglach@gmail.com>
2016-10-19 16:39:33 -0400

    refactored API calls for starred and contributed to lesson plans

--- app/controllers/lesson_plan_stars_controller.rb
+++ app/controllers/lesson_plan_stars_controller.rb
@@ -1,7 +1,7 @@
 class LessonPlanStarsController < ApplicationController
 
   def index
-    @teacher = Teacher.find(params[:teacher_id])
+    @teacher = Teacher.includes( {starred_lesson_plans: [:forked_plans, :teachers_who_starred] } ).find(params[:teacher_id])
     @lesson_plans = @teacher.starred_lesson_plans
     respond_to do |format|
       format.json {render "lesson_plans.json.jbuilder"}
