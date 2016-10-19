63c32a005fc82c0a102c95ce8870cdca7ade65ca
Graham Turner <turnertgraham@gmail.com>
2016-10-19 12:20:47 -0400

    removed n+1 query, added bullet gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20161018225025) do
 
   # These are extensions that must be enabled in order to support this database
@@ -42,10 +41,10 @@ ActiveRecord::Schema.define(version: 20161018225025) do
   end
 
   create_table "courses", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string   "title"
     t.integer  "teacher_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "lesson_plan_contributors", force: :cascade do |t|
@@ -195,6 +194,15 @@ ActiveRecord::Schema.define(version: 20161018225025) do
     t.datetime "avatar_updated_at"
     t.index ["email"], name: "index_teachers_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_teachers_on_reset_password_token", unique: true, using: :btree
+  end
+
+  create_table "testers", force: :cascade do |t|
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
