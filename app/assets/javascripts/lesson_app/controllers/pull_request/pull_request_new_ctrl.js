e5f08db6e0dfb2fcd39516e8e8fed96f6f0e2b0d
Philip Cole Johnson <philipcolejohnson@gmail.com>
2016-10-19 14:51:35 -0700

    Fixes injection bug

--- app/assets/javascripts/lesson_app/controllers/pull_request/pull_request_new_ctrl.js
+++ app/assets/javascripts/lesson_app/controllers/pull_request/pull_request_new_ctrl.js
@@ -1,4 +1,4 @@
-angular.module('Lesson').controller('PullRequestNewCtrl', ['$scope', '$stateParams', "pullRequestService", "LessonService", "lesson", "currentUser", function($scope, $stateParams, pullRequestService, LessonService, lesson, currentUser){
+angular.module('Lesson').controller('PullRequestNewCtrl', ['$scope', '$stateParams', "pullRequestService", "LessonService", "lesson", "currentUser", "teacher", "DiffService", function($scope, $stateParams, pullRequestService, LessonService, lesson, currentUser, teacher, DiffService){
 
   $scope.newPR = pullRequestService.getNewPullRequest($stateParams.id);
   $scope.forkedLesson = lesson;
