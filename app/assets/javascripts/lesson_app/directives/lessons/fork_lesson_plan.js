a3ebdb75dfdd9f41837d08432fdf99b3be92e275
Alex Lach <alexglach@gmail.com>
2016-10-20 17:29:03 -0400

    forks updating on front end

--- app/assets/javascripts/lesson_app/directives/lessons/fork_lesson_plan.js
+++ app/assets/javascripts/lesson_app/directives/lessons/fork_lesson_plan.js
@@ -1,7 +1,7 @@
-Lesson.directive("forkLessonPlan",  [ "LessonService", "$stateParams", "Auth", "$state", 'flash', '$timeout', function(LessonService, $stateParams, Auth, $state, flash, $timeout) {
+Lesson.directive("forkLessonPlan",  [ "LessonService", "$stateParams", "Auth", "$state", 'flash', '$timeout', '_', function(LessonService, $stateParams, Auth, $state, flash, $timeout, _) {
   return {
     template:"<button class='btn primary-bg-color pull-right' ng-click='forkLesson()'>Fork This Lesson</button>",
-    scope: { lesson: "=" },
+    scope: { owner: "=" },
     restrict: "E",
     link: function(scope) {
       // creates a new copy of the current lesson under the current user with updated version number
@@ -10,6 +10,7 @@ Lesson.directive("forkLessonPlan",  [ "LessonService", "$stateParams", "Auth", "
 
         LessonService.getLesson($stateParams.id).then(function(response){
 
+          _.find(scope.owner.lesson_plans, {'id': Number($stateParams.id)}).forks++
           return Auth.currentUser().then(function(currentUser){
             return LessonService.create( {title: response.title,
                                   content: response.content,
@@ -24,6 +25,7 @@ Lesson.directive("forkLessonPlan",  [ "LessonService", "$stateParams", "Auth", "
                                   grade: response.grade } )
 
           .then(function(newLesson) {
+            newLesson.parent_plan_title = newLesson.title;
             $state.go("main.lessons.show", {id: newLesson.id})
             $timeout(function(){
               flash('alert-success', 'Lesson plan forked!') 
