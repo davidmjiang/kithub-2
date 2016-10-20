a195a0bd59ee207eed1ca16d6aa7cfc1830ccaca
lynchd2 <lynchd2@tcnj.edu>
2016-10-20 13:12:27 -0400

    Fix overall score

--- app/assets/javascripts/gradebook_app/controllers/students/student_destroy_ctrl.js
+++ app/assets/javascripts/gradebook_app/controllers/students/student_destroy_ctrl.js
@@ -8,8 +8,8 @@ Gradebook.controller("StudentDestroyCtrl", ["$scope", "course", "_", "StudentSer
 			for(var i = 0; i < $scope.students.length; i++) {
 				//Because one is a string and one is a number, '==' will check for equality without type
 				if($scope.students[i].id == studentId) {
+					$scope.students.splice(i, 1);
 					StudentService.removeStudent($scope.students[i]).then(function(removedStudent) {
-						$scope.students.splice(i, 1);
 						$rootScope.$broadcast("student.deleted", removedStudent);
 						$scope.close();
 					})
