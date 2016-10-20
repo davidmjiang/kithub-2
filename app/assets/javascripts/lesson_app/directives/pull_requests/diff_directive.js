cb791eed4ed2b7069ee86f1b62100802094e2e9e
Hannah Squier <hannahsquier@gmail.com>
2016-10-20 15:24:40 -0700

    fix pull request count to only reflect pending prs

--- app/assets/javascripts/lesson_app/directives/pull_requests/diff_directive.js
+++ app/assets/javascripts/lesson_app/directives/pull_requests/diff_directive.js
@@ -6,7 +6,6 @@ Lesson.directive("diff",  [ "pullRequestService", "DiffService", "LessonService"
       restrict: "E",
 
       link: function(scope) {
-        console.log(scope.pullRequest)
         if (!scope.pullRequest.parent_plan) {
           LessonService.getLesson(scope.pullRequest.parent_plan_id).then(function(response) {
             pullRequest.parent_plan = response
