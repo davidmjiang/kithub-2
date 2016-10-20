0a253bb6a5bb49ac739701f6861804ec9e80cdb7
“Matthew <matthew.hinea@gmail.com>
2016-10-20 15:37:05 -0700

    fix bug causing backdrop clicks on assignment show modals to prevent tab navigation in other assignment show modals

--- app/assets/javascripts/gradebook_app/controllers/courses/course_show_ctrl.js
+++ app/assets/javascripts/gradebook_app/controllers/courses/course_show_ctrl.js
@@ -218,6 +218,12 @@ Gradebook.controller('CourseShowCtrl', ['$scope', 'course', "StudentService", "A
         students: $scope.students
       }
     }).then(function(modal) {
+
+      modal.element.one('hidden.bs.modal', function () {
+        if (!modal.controller.closed) {
+          modal.controller.closeModal();
+        }
+      });
       modal.element.modal();
       modal.close.then(function(response) {
 
