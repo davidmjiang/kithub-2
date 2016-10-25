9340111e9164ab326c3d2e91a84900ffa68fa2d1
David Jiang <davidjgrey@gmail.com>
2016-10-25 10:46:35 -0700

    download lesson button with helptext

--- app/controllers/lesson_plans_controller.rb
+++ app/controllers/lesson_plans_controller.rb
@@ -72,7 +72,7 @@ class LessonPlansController < ApplicationController
   end
 
   def export
-    @lesson = current_teacher.lesson_plans.find(params[:lesson_plan_id])
+    @lesson = LessonPlan.find(params[:lesson_plan_id])
     content_with_headers = DocConvert.add_headers_to_markdown(@lesson)
     path = DocConvert.markdown_to_rtf(content_with_headers, @lesson.title)
     send_file path, :disposition => 'attachment'
