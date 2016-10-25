3a56c9a7230f7ddca9078741b7f36920556b6baa
Graham Turner <turnertgraham@gmail.com>
2016-10-25 18:32:39 -0400

    removed parents and student_parents join

--- app/models/student.rb
+++ app/models/student.rb
@@ -5,8 +5,6 @@ class Student < ApplicationRecord
   #many to many relationship for courses
   has_many :student_courses, dependent: :destroy
   has_many :courses, through: :student_courses
-  has_many :parents, through: :student_parents
-  has_many :student_parents, dependent: :destroy
 
   #many to many relationship for assignments
   has_many :submissions, dependent: :destroy
