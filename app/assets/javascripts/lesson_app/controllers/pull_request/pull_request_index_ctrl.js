3d8827ea9099c57f98acc44f44c23bfd6de40e5f
Philip Cole Johnson <philipcolejohnson@gmail.com>
2016-10-26 14:38:28 -0700

    Removes pull request upon successful acceptance/rejection

--- app/assets/javascripts/lesson_app/controllers/pull_request/pull_request_index_ctrl.js
+++ app/assets/javascripts/lesson_app/controllers/pull_request/pull_request_index_ctrl.js
@@ -1,12 +1,8 @@
 angular.module('Lesson').controller('PullRequestIndexCtrl', ['$scope', 'currentUser', "pullRequestService", "_", function($scope, currentUser, pullRequestService, _){
 
-  $scope.pullRequests = pullRequestService.getPullRequests();
-
-  $scope.currentUser = currentUser.id;
+  // $scope.pullRequests = pullRequestService.getPullRequests();
+  // $scope.currentUser = currentUser.id;
 
   $scope.pendingPRs = pullRequestService.getPendingPRs();
-
-  console.log($scope.pullRequests)
-  console.log($scope.pendingPRs)
 
 }]);
