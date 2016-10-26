dac9e35e6524f391d86d17a34b78954216d2bb6e
Hannah Squier <hannahsquier@gmail.com>
2016-10-26 15:23:06 -0700

    more styling

--- app/assets/javascripts/lesson_app/controllers/teacher/teacher_following_ctrl.js
+++ app/assets/javascripts/lesson_app/controllers/teacher/teacher_following_ctrl.js
@@ -2,7 +2,6 @@
 angular.module('Lesson').controller('TeacherFollowingCtrl', ['$scope', 'currentUser', '$stateParams', 'FollowingService', 'populate', function($scope, currentUser, $stateParams, FollowingService){
 
 	$scope.following = FollowingService.getFollowedBy()[$stateParams.id];
-
   $scope.currentUser = currentUser;
 
 }]);
