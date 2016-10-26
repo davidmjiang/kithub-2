06476321b4bd19ad6161c06daa4bdca361942860
Philip Cole Johnson <philipcolejohnson@gmail.com>
2016-10-26 14:03:31 -0700

    Moved pending forks to a service

--- app/assets/javascripts/lesson_app/controllers/pull_request/pull_request_index_ctrl.js
+++ app/assets/javascripts/lesson_app/controllers/pull_request/pull_request_index_ctrl.js
@@ -1,10 +1,12 @@
-angular.module('Lesson').controller('PullRequestIndexCtrl', ['$scope', 'currentUser', "pullRequestService", "_", "pullRequests", function($scope, currentUser, pullRequestService, _, pullRequests){
+angular.module('Lesson').controller('PullRequestIndexCtrl', ['$scope', 'currentUser', "pullRequestService", "_", function($scope, currentUser, pullRequestService, _){
 
   $scope.pullRequests = pullRequestService.getPullRequests();
 
-  $scope.currentUser = currentUser.id
+  $scope.currentUser = currentUser.id;
 
-  $scope.pendingPRs = _.find($scope.pullRequests, function(pr) {
-          return pr.status === "pending" });
+  $scope.pendingPRs = pullRequestService.getPendingPRs();
+
+  console.log($scope.pullRequests)
+  console.log($scope.pendingPRs)
 
 }]);
