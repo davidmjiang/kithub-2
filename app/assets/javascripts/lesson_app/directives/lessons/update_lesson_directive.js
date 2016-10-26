673a8cd4f78d9cd58b653f7b8faa0588b4a63ae6
Philip Cole Johnson <philipcolejohnson@gmail.com>
2016-10-26 11:18:17 -0700

    Accepts (or rejects) changes from parent

--- app/assets/javascripts/lesson_app/directives/lessons/update_lesson_directive.js
+++ app/assets/javascripts/lesson_app/directives/lessons/update_lesson_directive.js
@@ -1,45 +1,48 @@
 Lesson.directive("updateLessonModal",  [ "DiffService", 'LessonService', "Restangular", "$window", function(DiffService, LessonService, Restangular, $window) {
   return {
     templateUrl:"lesson_templates/lessons/update_lesson_modal.html",
-    scope: { lesson: "=", currentUser: "=", diffs: "=" },
+    scope: { lesson: "=", currentUser: "=", diffs: "=", parent: "=" },
     restrict: "E",
     link: function(scope) {
       scope.lessonBelongsToCurrentUser = (scope.currentUser.id === scope.lesson.teacher_id) ? true : false;
-      // scope.comments = scope.pullRequest.comments;
-      // scope.diffs = DiffService.getDiffs(scope.pullRequest.parent_plan.content, scope.pullRequest.forked_plan.content);
+      console.log(scope.lesson)
+      setTimeout(function() {console.log(scope.parent)}, 500)
 
-      // LessonService.getLesson($stateParams.id).then(function(lesson) {
-      //   Auth.currentUser().then(function(currentUser){
-      //     if (currentUser.id === lesson.teacher_id) {
-      //       scope.lessonBelongsToCurrentUser = true;
-      //     } else {
-      //       scope.lessonBelongsToCurrentUser = false;
-      //     }
-      //   });
-      // });
+      //Porbably need to update version, make pull requests as accepted somehow.
+      scope.acceptChanges = function() {
+        var oldContent = scope.lesson.content;
+        var oldVersion = scope.lesson.parent_version;
+        scope.lesson.content = DiffService.acceptChanges(scope.diffs);
+        // angular.element(".modal-backdrop").remove();
+        
+        scope.lesson.parent_version = scope.parent.version;
+        console.log(oldVersion + " -> " + scope.lesson.parent_version)
+        LessonService.save(scope.lesson).then(
+          function() {
+            LessonService.setFlash('alert-success', 'Lesson updated!');
+          },
+          function() {
+            LessonService.setFlash('alert-danger', 'Could not update lesson');
+            scope.lesson.content = oldContent;
+            scope.lesson.parent_version = oldVersion;
+        });
+      };
 
-      // //Porbably need to update version, make pull requests as accepted somehow.
-      // scope.acceptChanges = function() {
-      //   var newContent = DiffService.acceptChanges(scope.diffs);
-      //   angular.element(".modal-backdrop").remove();
-      //   scope.pullRequest.parent_plan.content = newContent;
-      //   scope.pullRequest.parent_plan = Restangular.restangularizeElement(null, scope.pullRequest.parent_plan, "lesson_plans");
-
-      //   contributorData = { teacher_id: scope.pullRequest.forked_plan.teacher_id,
-      //                       lesson_plan_id: scope.pullRequest.parent_plan.id };
-
-      //   LessonService.save(scope.pullRequest.parent_plan).then(function() {
-      //     pullRequestService.acceptChanges(scope.pullRequest, contributorData, $stateParams.id);
-      //     scope.lesson.content = newContent;
-      //   });
-      //   $window.location.reload();
-      // };
-
-      // scope.rejectChanges = function() {
-      //   angular.element(".modal-backdrop").remove();
-      //   pullRequestService.rejectChanges(scope.pullRequest, $stateParams.id);
-      //   $window.location.reload();
-      // };
+      scope.rejectChanges = function() {
+        var oldVersion = scope.lesson.parent_version;
+        // angular.element(".modal-backdrop").remove();
+        
+        scope.lesson.parent_version = scope.parent.version;
+        console.log(oldVersion + " -> " + scope.lesson.parent_version)
+        LessonService.save(scope.lesson).then(
+          function() {
+            LessonService.setFlash('alert-success', 'Lesson updated!');
+          },
+          function() {
+            LessonService.setFlash('alert-danger', 'Could not update lesson');
+            scope.lesson.parent_version = oldVersion;
+        });
+      };
     }
   };
 
