d271046c4dff597f5bc03731ab167735cbc688a1
David Jiang <davidjgrey@gmail>
2016-10-26 15:22:50 -0700

    tabs will show active state on refresh

--- app/assets/javascripts/lesson_app/controllers/teacher/starred_lessons_ctrl.js
+++ app/assets/javascripts/lesson_app/controllers/teacher/starred_lessons_ctrl.js
@@ -1,5 +1,7 @@
 "use strict";
-angular.module('Lesson').controller('StarredLessonsCtrl', ['$scope', 'currentUser', 'starred_lessons', function($scope, currentUser, starred_lessons){
+angular.module('Lesson').controller('StarredLessonsCtrl', ['$scope', 'currentUser', 'starred_lessons', '$rootScope', function($scope, currentUser, starred_lessons, $rootScope){
+
+	$rootScope.$broadcast("on:page", "starred");
 
   $scope.starred_lessons = starred_lessons
 
--- app/assets/javascripts/lesson_app/controllers/teacher/teacher_following_ctrl.js
+++ app/assets/javascripts/lesson_app/controllers/teacher/teacher_following_ctrl.js
@@ -1,5 +1,7 @@
 "use strict";
-angular.module('Lesson').controller('TeacherFollowingCtrl', ['$scope', 'currentUser', '$stateParams', 'FollowingService', 'populate', function($scope, currentUser, $stateParams, FollowingService){
+angular.module('Lesson').controller('TeacherFollowingCtrl', ['$scope', 'currentUser', '$stateParams', 'FollowingService', '$rootScope', function($scope, currentUser, $stateParams, FollowingService, $rootScope){
+
+	$rootScope.$broadcast("on:page", "following");
 
 	$scope.following = FollowingService.getFollowedBy()[$stateParams.id];
 
