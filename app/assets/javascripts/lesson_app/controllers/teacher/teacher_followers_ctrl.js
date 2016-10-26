d0e3701cf4f33982b964367f55fe497f43b5cc03
David Jiang <davidjgrey@gmail.com>
2016-10-26 15:22:50 -0700

    tabs will show active state on refresh

--- app/assets/javascripts/lesson_app/controllers/teacher/teacher_followers_ctrl.js
+++ app/assets/javascripts/lesson_app/controllers/teacher/teacher_followers_ctrl.js
@@ -1,5 +1,7 @@
 "use strict";
-angular.module('Lesson').controller('TeacherFollowersCtrl', ['$scope', 'currentUser', '$stateParams', 'FollowingService', 'populate', function($scope, currentUser, $stateParams, FollowingService){
+angular.module('Lesson').controller('TeacherFollowersCtrl', ['$scope', 'currentUser', '$stateParams', 'FollowingService', '$rootScope', function($scope, currentUser, $stateParams, FollowingService, $rootScope){
+
+	$rootScope.$broadcast("on:page", "followers");
 
   $scope.followers = FollowingService.getFollowersOf()[$stateParams.id];
 
