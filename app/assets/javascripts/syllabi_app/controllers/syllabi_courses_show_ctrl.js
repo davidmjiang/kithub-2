2dbbdad2f25da3c37126c314f017b34447a95535
Alex Lach <alexglach@gmail.com>
2016-10-25 23:18:48 -0400

    filters working on show page

--- app/assets/javascripts/syllabi_app/controllers/syllabi_courses_show_ctrl.js
+++ app/assets/javascripts/syllabi_app/controllers/syllabi_courses_show_ctrl.js
@@ -40,6 +40,43 @@ Syllabi.controller('SyllabiCoursesShowCtrl', ['$scope', '$state', 'currentUser',
       SyllabiCourseService.removeLessonPlanDay(courseDay.id, lesson_plan.id);
     }
 
+    $scope.filters = {
+      hourMin: 0,
+      hourMax: 10,
+      gradeMin: 0,
+      gradeMax: 12,
+      subject: "",
+      lessonType: "",
+      state: "",
+      options: {
+        hideLimitLabels: true
+      }
+    };
+
+    $scope.between = function(prop, min, max){
+      return function(item){
+        return item[prop] >= min && item[prop] <= max;
+      }
+    };
+
+    $scope.resetFilters = function(){
+      $scope.filters.hourMin = 0;
+      $scope.filters.hourMax = 10;
+      $scope.filters.gradeMin = 0;
+      $scope.filters.gradeMax = 12;
+      $scope.filters.subject = "";
+      $scope.filters.lessonType = "";
+      $scope.filters.state = "";
+    };
+
+     $scope.states = ['Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Florida','Georgia','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyoming'];
+
+    $scope.grades = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+    $scope.subjects = ['Math', 'Science', 'English', 'Foreign Language', 'Art', 'Physical Education', 'History'];
+
+    $scope.lessonTypes = ['Individual', 'Group', 'Teacher-led'];
+
 
 
 
