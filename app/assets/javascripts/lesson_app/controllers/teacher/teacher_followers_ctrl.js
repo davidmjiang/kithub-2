125b512f6b16c7c7b378217cce09c8f91391819a
Graham Turner <turnertgraham@gmail.com>
2016-10-26 16:52:10 -0400

    fixed following/ers bug

--- app/assets/javascripts/lesson_app/controllers/teacher/teacher_followers_ctrl.js
+++ app/assets/javascripts/lesson_app/controllers/teacher/teacher_followers_ctrl.js
@@ -1,6 +1,6 @@
 "use strict";
-angular.module('Lesson').controller('TeacherFollowersCtrl', ['$scope', 'currentUser', '$stateParams', 'FollowingService', function($scope, currentUser, $stateParams, FollowingService){
-	
+angular.module('Lesson').controller('TeacherFollowersCtrl', ['$scope', 'currentUser', '$stateParams', 'FollowingService', 'populate', function($scope, currentUser, $stateParams, FollowingService){
+
   $scope.followers = FollowingService.getFollowersOf()[$stateParams.id];
 
   $scope.currentUser = currentUser;
